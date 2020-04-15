1b2f131ed7ba326fd3e6889c1c83467cb52c6b48
leandrovi <leandrofernandesvieira@gmail.com>
2020-04-15 13:57:19 -0300

    chore: setup prettier

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -2,6 +2,8 @@ import { Router } from 'express';
 
 const routes = Router();
 
-routes.get('/', (request, response) => response.send({ message: 'Hello GoStack!' }));
+routes.get('/', (request, response) =>
+  response.send({ message: 'Hello GoStack!' }),
+);
 
 export default routes;
