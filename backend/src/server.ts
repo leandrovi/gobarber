b6053e5c12eb0c7363bb3c4bd1e196ae4bafb475
leandrovi <leandrofernandesvieira@gmail.com>
2020-04-27 21:29:08 -0300

    chore: add cors to api

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata';
 
 import express, { Request, Response, NextFunction } from 'express';
+import cors from 'cors';
 import 'express-async-errors';
 
 import routes from './routes';
@@ -11,6 +12,10 @@ import './database';
 
 const app = express();
 
+// app.use(cors({
+//   origin: 'https://www.gobarber.com.br'
+// }));
+app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
