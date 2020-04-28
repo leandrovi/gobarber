ce6386f3e0161a27622ec3b2a7627e50531b02f8
leandrovi <leandrofernandesvieira@gmail.com>
2020-04-27 22:18:59 -0300

    feat: login via context

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,18 +4,16 @@ import SignIn from './pages/SignIn';
 // import SignUp from './pages/SignUp';
 import GlobalStyle from './styles/global';
 
-import AuthContext from './context/AuthContext';
+import { AuthProvider } from './context/AuthContext';
 
-const App: React.FC = () => {
-  return (
-    <>
-      <AuthContext.Provider value={{ name: 'Leandro' }}>
-        <SignIn />
-      </AuthContext.Provider>
+const App: React.FC = () => (
+  <>
+    <AuthProvider>
+      <SignIn />
+    </AuthProvider>
 
-      <GlobalStyle />
-    </>
-  );
-};
+    <GlobalStyle />
+  </>
+);
 
 export default App;
