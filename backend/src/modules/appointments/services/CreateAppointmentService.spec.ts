ccd9357d06c2431e138ea2e26793a8e13337e44b
leandrovi <leandrofernandesvieira@gmail.com>
2020-06-08 21:55:58 -0300

    test: appointments tests

--- backend/src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ backend/src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,3 +1,5 @@
+import AppError from '@shared/errors/AppError';
+
 import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
 import CreateAppointmentService from './CreateAppointmentService';
 
@@ -17,7 +19,24 @@ describe('CreateAppointment', () => {
     expect(appointment.provider_id).toBe('12345');
   });
 
-  // it('should not be able to create two appointments in the same datetime', () => {
-  //   expect(1 + 2).toBe(3);
-  // });
+  it('should not be able to create two appointments in the same datetime', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
+
+    const appointmentDate = new Date(2020, 5, 8, 9);
+
+    await createAppointment.execute({
+      date: appointmentDate,
+      provider_id: '12345',
+    });
+
+    expect(
+      createAppointment.execute({
+        date: appointmentDate,
+        provider_id: '12345',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
 });
